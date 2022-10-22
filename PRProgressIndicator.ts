03c7d055cf8835da7d786d73fbb3add65607638c
RoySegall <roy@testim.io>
2022-10-22 10:54:55 +0300

    a

--- PRProgressIndicator.ts
+++ PRProgressIndicator.ts
@@ -2,6 +2,6 @@ import {createOrUpdateProgressIndicators} from "./api/utils";
 import * as github from '@actions/github';
 
 (async () => {
-    console.log(github.context.issue);
+    console.log(github.context);
     await createOrUpdateProgressIndicators();
 })();
