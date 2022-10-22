202362a26c9b3b7304f57ff52b698c6ab892c06c
RoySegall <roy@testim.io>
2022-10-22 15:42:47 +0300

    Trhying now

--- PRProgressIndicator.ts
+++ PRProgressIndicator.ts
@@ -1,18 +1,6 @@
-import {createOrUpdateProgressIndicators, getOctokitClient} from "./api/utils";
-import * as github from '@actions/github';
+import {createOrUpdateProgressIndicators} from "./api/utils";
 
 (async () => {
-
-    const foo = await getOctokitClient().request('GET /repos/{owner}/{repo}/actions/runs/{run_id}', {
-        owner: 'roysegall',
-        repo: 'testim-gh-labels-handler',
-        run_id: github.context.runId
-    })
-
-    if (foo.data.pull_requests) {
-        console.log(foo.data.pull_requests);
-
-    }
     await createOrUpdateProgressIndicators();
-    process.exit(0)
+    process.exit(1)
 })();
