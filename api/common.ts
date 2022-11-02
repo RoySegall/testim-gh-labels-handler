cf75606c88cb24c2daaca7f39d46c204a3fae2fd
RoySegall <roy@testim.io>
2022-11-02 06:23:35 +0200

    more notes

--- api/common.ts
+++ api/common.ts
@@ -1,4 +1,4 @@
-import {PullRequestInfo} from "./interfaces";
+import {PullRequestInfo, Releases} from "./interfaces";
 import {Octokit} from "@octokit/core";
 
 export const owner = 'roysegall';
@@ -6,10 +6,17 @@ export const repo = 'testim-gh-labels-handler';
 export const octokitClient = new Octokit({ auth: process.env.ghToken });
 export const PR_ID = process.env.PR_ID ? parseInt(process.env.PR_ID) : 0;
 
-export async function getPRInfo(owner: string, repo: string, pull_number: number): Promise<PullRequestInfo> {
+export async function getPRInfo(pull_number: number): Promise<PullRequestInfo> {
     const { data: PRInfo } = await octokitClient.request('GET /repos/{owner}/{repo}/pulls/{pull_number}', {
         owner, repo, pull_number,
     });
 
     return PRInfo;
+}
+
+export async function getReleases(): Promise<Releases> {
+    const {data} = await octokitClient.request('GET /repos/{owner}/{repo}/releases', {
+        repo, owner
+    });
+    return data;
 }
--- api/drafter.ts
+++ api/drafter.ts
@@ -1,18 +1,19 @@
-import {getPRInfo, owner, PR_ID, repo} from "./common";
-import {Label} from "./interfaces";
+import {getPRInfo, getReleases, PR_ID} from "./common";
+import {Label, Releases} from "./interfaces";
 
+type LocationInNotes = 'feature' | 'fix' | 'maintaince';
 type DraftState = {
     clickim?: boolean,
     runner?: boolean,
     services?: boolean,
     webdriverio?: boolean,
-    locationInNotes?:  'feature' | 'fix' | 'maintaince';
+    locationInNotes?: LocationInNotes;
 };
 
 function determinesDraftsByLabels(labels: Label[]) {
     const draftState: DraftState = {};
 
-    const handlers: {[key: string]: () => void} = {
+    const handlers: { [key: string]: () => void } = {
         clickim: () => draftState.clickim = true,
         runner: () => draftState.runner = true,
         services: () => draftState.services = true,
@@ -34,8 +35,13 @@ function determinesDraftsByLabels(labels: Label[]) {
     return draftState;
 }
 
-function createOrGetDraftForEdit(draftStates: DraftState): object[] {
-    const draftsToEdit: {}[] = [];
+async function createOrGetDraftForEdit(draftStates: DraftState): Promise<Releases[]> {
+    const draftsToEdit: Releases[] = [];
+
+    const releases = await getReleases();
+    const draftsTitle = Object.fromEntries(releases
+        .filter(release => release.draft)
+        .map(release => [release.name!.replace('-draft', ''), release]));
 
     Object.keys(draftStates).forEach(draftState => {
         if (draftState === 'locationInNotes') {
@@ -43,27 +49,30 @@ function createOrGetDraftForEdit(draftStates: DraftState): object[] {
             return;
         }
 
-        console.log(`Checking on ${draftState}`)
+        if (Object.keys(draftsTitle).includes(draftState)) {
+            console.log(`No need to create a release for ${draftState}. Skipping`);
+            return;
+        }
+
+        console.log(`Creating a draft release for ${draftState}`)
     });
 
-
-    // if not, create a draft and return the created one.
 
     return draftsToEdit;
 }
 
-function attachPrInfoToDraftBody(draft: object) {
+function updateDraft(drafts: Releases[], title: string, user: string, locationInNotes: LocationInNotes) {
     // Get the body of the draft.
     // Search for the proper category.
     // update the draft with the new text.
 }
 
 (async () => {
-    const {title, user, labels} = await getPRInfo(owner, repo, PR_ID);
+    const {title, user, labels} = await getPRInfo(PR_ID);
 
     const draftState = determinesDraftsByLabels(labels);
-    const draft = await createOrGetDraftForEdit(draftState);
-    attachPrInfoToDraftBody(draft);
+    const releases = await createOrGetDraftForEdit(draftState);
+    updateDraft(releases, title, user?.name!, draftState.locationInNotes!);
 
     // The draft are the first ones. Check if we have ones by the items. todo: what about webdriver io draft?
 })();
